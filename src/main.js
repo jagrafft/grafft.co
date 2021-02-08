3cc68cd6e7f02827ba99cc468805f20f45278fdf
j.a.g <jgrafft@gmail.com>
2021-02-08 18:07:17 +0000

    Added content.

--- src/main.js
+++ src/main.js
@@ -3,7 +3,40 @@ import App from "./App.svelte"
 const app = new App({
     target: document.body,
     props: {
-        copyDate: new Date().getFullYear()
+        footer: {
+            acknowledgements: [
+                { label: "CertBot", url: "" },
+                { label: "FontPair", url: "" },
+                { label: "Montserrat (head)", url: "" },
+                { label: "Crimson Text (body)", url: "" },
+                { label: "Home Icon Html #352283", url: "" },
+                { label: "NGINX", url: "" },
+                { label: "Open Color", url: "" },
+                { label: "Svelte.js", url: "" },
+                { label: "zero-md", url: "" }
+            ],
+            copyDate: new Date().getFullYear(),
+            copyHolder: "jason a. grafft"
+            copyEmail: "",
+            copyEmailLabel: "jason &#x2200;&#x22A5 grafft.co"
+        },
+        masthead: {
+            bannerText: "the personal website of jason a. grafft",
+            image: "img/AnscombesQuartet.png",
+            imageAltText: "Anscombe's Quartet",
+            imageCaption: "Integrity is not certainty."
+        },
+        navbar: {
+            items: [
+                { label: "", url: "" },
+                { label: "about", url: "" },
+                { label: "cv", url: "" },
+                { label: "presentations", url: "" },
+                { img: "", url: "" },  // GitHub
+                { img: "", url: "" }  // Observable
+                // { img: "", url: "" }  // LinkedIn
+            ]
+        }
     }
 });
 
